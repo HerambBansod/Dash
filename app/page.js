7ef7392588700fd2e162b1ba971e4ea95037d854
Heramb Bansod <heramb.b744@gmail.com>
2026-02-13 10:18:46 +0530

    init dash commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <main className="bg-[#cccccc] w-full min-w-[1330px] h-screen flex items-center justify-center">
+    <div>
+        <h1 className="text-black ">We Working on Dashboard</h1>
+    </div>
+  </main>
+  );
+}
